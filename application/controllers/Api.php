2357b5422e009db216501958a8bd9f3653184206
unknown <vrushalrt@gmail.com>
2018-09-24 11:13:03 +0530

    First commit lovzmeprice api implemented R:TAX rate issue

---
+++ application/controllers/Api.php
@@ -0,0 +1,76 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('products');
+	}
+
+	public function index()
+	{
+		echo "Lovzme API";
+	}
+
+	public function products()
+	{
+		$id_product = $this->uri->segment(3);
+		
+		if(isset($id_product))
+		{
+			$this->products->getProducts($id_product);
+		}	
+		else
+		{
+			$this->products->getProducts();
+		}
+	}
+
+	public function lovzmeprice()
+	{
+		$id_product = $this->uri->segment(3);
+		
+		if(isset($id_product))
+		{
+			$this->products->getLovzmeProductsPrice($id_product);
+		}
+		else
+		{
+			$this->products->getLovzmeProductsPrice();
+		}
+	}
+
+	public function getmethod()
+	{
+		echo "Hello";
+		$aa = $this->uri->segment(3);
+		// echo $aa;
+		
+	}
+
+	public function test()
+	{
+		echo "Hello Test";
+		$url = file_get_contents("http://localhost/php/ci/lovzmeapi/index.php/api/lovzmeprice/14");
+		$data = json_decode($url,true);
+		// print_r($data);
+	}
+}
---
+++ application/models/products.php
@@ -0,0 +1,111 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Products extends CI_Model
+{
+	public function __construct()
+	{
+		header('Content-Type: application/json; charset=utf-8');
+	}
+
+	public function getProducts($id_product=null)
+	{
+		if(!empty($id_product))
+		{
+			$this->db->where('id_product',$id_product);
+			$query = $this->db->get(_DB_PREFIX_.'product');
+			echo json_encode($query->result());
+		}
+		else
+		{
+			$query = $this->db->get(_DB_PREFIX_.'product');
+			echo json_encode($query->result());
+		}
+	}
+
+	public function getLovzmeProductsPrice($id_product)
+	{	
+		if(!empty($id_product))
+		{
+			// $aa = array('1','2','3');
+			echo json_encode($this->calProductPriceTax($id_product));
+		}
+		else
+		{
+			$this->db->select('id_product');
+			$result = $this->db->get(_DB_PREFIX_.'product')
+								->result();
+
+			$json = array();
+			foreach ($result as $key => $row) 
+			{
+				$json[] = $this->calProductPriceTax($row->id_product);
+			}
+			echo json_encode($json);
+		}
+	}
+
+	public function calProductPriceTax($id_product)
+	{
+			if(!empty($id_product))
+			{
+				// Getting TAX RULE GROUP
+				$this->db->select('id_tax_rules_group,price');
+				$this->db->where('id_product',$id_product);
+				$products = $this->db->get(_DB_PREFIX_.'product')
+									->row();
+
+				$id_tax_rules_group = $products->id_tax_rules_group;
+				$price = $products->price;
+				
+				// Getting TAX ID
+				$this->db->select('id_tax');
+				$this->db->where('id_tax_rules_group',$id_tax_rules_group);
+				$id_tax = $this->db->get(_DB_PREFIX_.'tax_rule')	
+										->row()
+										->id_tax;
+				
+				// Gettring TAX RATE
+				$this->db->select('rate');
+				$this->db->where('id_tax',$id_tax);
+				$tax_rate = $this->db->get(_DB_PREFIX_.'tax')
+										->row()
+										->rate;
+				
+				
+				$data['id_product']	= $id_product;
+				$data['price'] = $price;
+				$data['price_tax_excl'] = $price;
+				$data['price_tax_incl'] = $price + ((($tax_rate + $tax_rate)/100) * $price);
+				$data['tax_rate'] = $tax_rate;
+				
+				// Getting Reducton rate
+				$this->db->select('reduction');
+				$this->db->where('id_product',$id_product);
+				$reduction_rate = $this->db->get(_DB_PREFIX_.'specific_price')
+												->row();
+
+				$reduction_rate = floatval($reduction_rate->reduction);
+				if (isset($reduction_rate) && $reduction_rate != 0) 
+				{
+					$data['price_reduction'] = $data['price_tax_incl'] - (($reduction_rate) * $data['price_tax_incl']);
+					$data['reduction_rate'] = $reduction_rate * 100;
+				}
+				else
+				{
+					$data['reduction_rate'] = 0;
+				}
+
+				//echo json_encode($data);
+				// print_r($data);
+				return $data;
+
+			}
+			else
+			{
+				$data['error'] = "Poduct ID is not valid";
+				echo json_encode($data);
+			}
+	}
+	
+}
